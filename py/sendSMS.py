ce88b78703f43c22824dd7cd967ea7750d34d9d1
Mark Rodighiero <markatango@gmail.com>
2024-07-29 08:31:14 -0700

    cleaned up indents

--- py/sendSMS.py
+++ py/sendSMS.py
@@ -7,23 +7,22 @@ class SendSMS:
 		self.auth_token = os.environ.get('TWILIO_TOKEN')
 		self.fromph = os.environ.get('TWILIO_PHONE_FROM')
 		self.toph = toPh
+		print(self.auth_token)
+		print(self.account_sid)
+		print(self.fromph)
+		print(self.toph)
 
-                print(self.auth_token)
-                print(self.account_sid)
-                print(self.fromph)
-                print(self.toph)
-
-                self.client = Client(self.account_sid, self.auth_token)
-	def send(self, msg):
- 		message = self.client.messages.create(
-							 body = msg,
-							 from_= self.fromph,
-							 to = self.toph 
-						 )
-						 
-		print(message.sid)
+		self.client = Client(self.account_sid, self.auth_token)
 	
+	def send(self, msg):
+		message = self.client.messages.create(
+			body = msg,
+			from_= self.fromph,
+			to = self.toph 
+			)
+		print(message.sid)
+
 if __name__ == "__main__":
- 	to = os.environ.get('TWILIO_PHONE_TO')
+	to = os.environ.get('TWILIO_PHONE_TO')
 	s = SendSMS(to)
 	s.send("hello")
